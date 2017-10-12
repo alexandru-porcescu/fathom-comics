ac01cf8f9152de0dc3b37c952af227c68de3827b
Erik Rose <erik@mozilla.com>
2017-10-12 16:27:51 -0400

    Add RunAgainstCorpus abstraction. Get tuning loop running through without crashing.
    There's nothing to actually tune yet.
    
    Discovered jsdom was still pawing around to load referenced resources (and crashing, because there are some in the corpus with "null" for every field). Stopped that with `FetchExternalResources: false`.

--- tune.js
+++ tune.js
@@ -1,9 +1,32 @@
-function getDomsAndAnswers() {
-    return [1, 2, 3];
+const {readFileSync, readdirSync, statSync} = require('fs');
+const {jsdom} = require('jsdom/lib/old-api');
+const {join} = require('path');
+
+const {dom, props, out, rule, ruleset, score, type} = require('fathom-web');
+
+
+/**
+ * Parse an HTML doc, and return a DOM-compliant interface to it. Do not
+ * execute any of its inline scripts.
+ */
+function staticDom(html) {
+    return jsdom(html, {features: {ProcessExternalResources: false,
+                                   FetchExternalResources: false}});
 }
 
-function howManyWrong(domsAndAnswers, coeffs) {
-    return 1;
+function comicRuleset(coeffFoo = 0, coeffBar = 0) {
+    return ruleset(
+        rule(dom('img'), type('smoo').score(fnode => fnode.element.hasAttribute('src') ? fnode.element.getAttribute('src').length : 0)),
+        rule(type('smoo').max(), out('comic'))
+    );
+}
+
+/**
+ * @return {String[]} The name (not path) of each directory directly within a
+*      given path
+ */
+function dirsIn(path) {
+  return readdirSync(path).filter(f => statSync(join(path, f)).isDirectory());
 }
 
 if (require.main === module) {
@@ -13,12 +36,72 @@ if (require.main === module) {
     const {argv} = require('process');
 
     // Best-scoring coeffs so far:
-    let coeffs = [0, 0, 0, 0];
+    let coeffs = [0, 0];
+
+    /**
+     * Build up a total score as we run the ruleset over a supervised corpus.
+     */
+    class RunAgainstCorpus {
+        /**
+         * @arg rules {Ruleset} A ruleset, tuned with the coefficients you want
+         *     to try
+         */
+        constructor(rules) {
+            this.rules = rules;
+            this.scoreParts = {number: 0, numberWrong: 0};
+        }
+
+        run() {
+            for (const sample of this.corpus(join(__dirname, 'corpus', 'training'))) {
+                this.updateScoreParts(sample);
+            }
+        }
+
+        /**
+         * @return An iterable of objects carrying the possibly onLoad-mutated
+         *     document HTML, the index of the tag that should represent the
+         *     comic, and a human-readable name for the sample.
+         */
+        *corpus(dir) {
+            for (const sampleDir of dirsIn(dir)) {
+                const html = readFileSync(join(dir, sampleDir, 'source.html'),
+                                          {encoding: 'utf8'});
+                yield {name: sampleDir,
+                       doc: staticDom(html)}
+            }
+        }
+
+        /**
+         * Run the ruleset over the single sample, and update scoreParts.
+         *
+         * @arg sample An arbitrary data structure that specifies which sample
+         *     from the corpus to run against and the expected answer
+         */
+        updateScoreParts(sample) {
+            const comic = this.rules.against(sample.doc).get('comic')[0];
+            if (comic.element.getAttribute('data-fathom-comic') !== 1) {
+                this.scoreParts.numberWrong += 1;
+                // Could we turn this into an "amount wrong" by saying how far (in index (which would consider some "second best" images crazy bad), or in space (maybe better)) the selected element is from the correct one? Maybe favor the cheaper distance function, or just try both and see which leads the optimizer to a set of coeffs with more right answers.
+                console.log('Wrong answer for ' + sample.name + ': ' + comic.innerHtml);
+            }
+            this.scoreParts.number += 1;
+        }
+
+        score() {
+            if (this.scoreParts.number === 0) {
+                this.run();
+            }
+            return this.scoreParts.numberWrong;
+        }
+
+        humanScore() {
+            return (1 - (this.score() / this.scoreParts.number)) * 100;
+        }
+    }
 
     class Tuner extends Annealer {
-        constructor() {
-            super();
-            this.solutionCost = coeffs => howManyWrong(getDomsAndAnswers('training'), coeffs);
+        solutionCost(coeffs) {
+            return new RunAgainstCorpus(comicRuleset(...coeffs)).score();
         }
 
         randomTransition(solution) {
@@ -50,6 +133,5 @@ if (require.main === module) {
         coeffs = annealer.anneal();
     }
     console.log('Tuned coefficients:', coeffs);
-    const domsAndAnswers = getDomsAndAnswers('training');
-    console.log('% right:', 100 - (howManyWrong(domsAndAnswers, coeffs) / domsAndAnswers.length * 100));  // TODO: Replace with validation set once we get a decently high number.
+    console.log('% right:', new RunAgainstCorpus(comicRuleset()).humanScore());  // TODO: Replace with validation set once we get a decently high number.
 }
